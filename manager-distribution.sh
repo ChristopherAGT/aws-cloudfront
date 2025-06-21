ac1e31883018efd38e524c9c902f66f05a428df8
ChristopherAGT <christopherchivalan01@gmail.com>
2025-06-20 22:05:03 -0600

    Update manager-distribution.sh

--- manager-distribution.sh
+++ manager-distribution.sh
@@ -167,6 +167,7 @@ while true; do
         6)
             echo -e "${MAGENTA}👋 Saliendo del panel...${RESET}"
             echo -e "${CYAN}💡 Puedes ejecutar nuevamente el panel con el comando: ${BOLD}aws-manager${RESET}"
+            echo -e "${YELLOW}📝 Créditos a 👾 Christopher Ackerman${RESET}"
             exit 0
             ;;
         *)
